f90c68a5a1062f7beda98b6d7de1ce3eede0d3ce
LafyOjo <51814402+LafyOjo@users.noreply.github.com>
2020-09-26 22:50:17 +0100

    Add files via upload

---
+++ virus.js
@@ -0,0 +1,17 @@
+function Virus() {
+  this.x;
+  this.y;
+
+  this.pickLocation = function() {
+    this.x = (Math.floor(Math.random() *
+      columns - 1) + 1) * scale;
+    this.y = (Math.floor(Math.random() *
+      rows - 1) + 1) * scale;
+  }
+
+  this.draw = function() {
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(this.x, this.y, scale, scale)
+  }
+}
+ 
